900f94604d0a0823c1b6eb0933c6e126491b5452
Jaxh <1820804532@qq.com>
2020-04-08 11:25:05 +0800

    up

---
+++ php/htdocs/index.php
@@ -0,0 +1,8 @@
+<?php
+define('D', array(
+	"A" => 1,
+	"B" => 2
+));
+
+
+echo array_key_exists("C",D);
--- ts/main.js
+++ ts/main.js
@@ -1,8 +1,5 @@
 "use strict";
-var sms = require('source-map-support');
-sms.install();
-// pos = sms.mapSourcePosition({ source: "d:\\Documents\\Test\\ts\\main.js", line: 10, column: 0 })
-var pos = sms.mapSourcePosition({ source: "d:\\ts\\demo.js", line: 2, column: 0 });
-console.log(pos);
-console.log(pos);
-//# sourceMappingURL=main.js.map
+var a = [1, 2, 3];
+console.log(a.shift());
+console.log(a.pop());
+//# sourceMappingURL=Main.js.map
